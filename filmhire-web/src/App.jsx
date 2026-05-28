549028c2eb40eef6458f8baf660ed927b76b8ef0
HarryMaheshwari <premjee.2009.2003.2009@gmail.com>
2026-05-28 18:16:34 +0530

    kuch bhi

--- filmhire-web/src/App.jsx
+++ filmhire-web/src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import {Routes} from "react-router-dom"
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import RoleSelect from "./pages/auth/RoleSelect";
 
 function App() {
-  return (
-    <div>App</div>
-  )
+  return <>
+  
+  <Routes>
+    <Route path="/login" element={<Login />} />
+    <Route path="/register" element={<Register />} />
+    <Route path="/select-role" element={<RoleSelect />} />
+      
+  </Routes>
+
+  </>;
 }
 
-export default App
+export default App;
