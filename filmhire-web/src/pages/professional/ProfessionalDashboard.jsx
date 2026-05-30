caf6b5b0206e70c257b2bfa6eb69f3567d05634e
HarryMaheshwari <premjee.2009.2003.2009@gmail.com>
2026-05-30 12:53:35 +0530

    added client and prof dashboard changes

--- filmhire-web/src/pages/professional/ProfessionalDashboard.jsx
+++ filmhire-web/src/pages/professional/ProfessionalDashboard.jsx
@@ -1,59 +1,50 @@
-import React, { useEffect } from 'react'
-import { supabase } from '../../lib/supabase';
-import { useNavigate } from 'react-router-dom';
-
-
+import React, { useEffect } from "react";
+import { supabase } from "../../lib/supabase";
+import { useNavigate } from "react-router-dom";
 
 const ProfessionalDashboard = () => {
-
-  const navigate = useNavigate()
-
+  const navigate = useNavigate();
 
   const handleLogout = async () => {
-  const { error } = await supabase.auth.signOut();
+    const { error } = await supabase.auth.signOut();
 
-  if (error) {
-    
-    return;
-  }
-
-  navigate("/login");
-};
-useEffect(() => {
-  const checkAccess = async () => {
-    const {
-      data: { user },
-    } = await supabase.auth.getUser();
-
-    console.log(user);
-    
-
-    if (!user) {
-      navigate("/login");
+    if (error) {
       return;
     }
 
-    const { data: profile } = await supabase
-      .from("profiles")
-      .select("role")
-      .eq("id", user.id)
-      .single();
-
-    if (profile?.role !== "professional") {
-      navigate("/client/dashboard");
-      return;
-    }
+    navigate("/login");
   };
+  useEffect(() => {
+    const checkAccess = async () => {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
 
-  checkAccess();
-}, []);
+      if (!user) {
+        navigate("/login");
+        return;
+      }
 
+      const { data: profile } = await supabase
+        .from("profiles")
+        .select("role")
+        .eq("id", user.id)
+        .single();
+
+      if (profile?.role !== "professional") {
+        navigate("/client/dashboard");
+        return;
+      }
+    };
+
+    checkAccess();
+  }, []);
 
   return (
     <div>
-        <button onClick={handleLogout}>Logout</button>
+      <button onClick={handleLogout}>Logout</button>
     </div>
-  )
-}
+  );
+};
 
-export default ProfessionalDashboard
+export default ProfessionalDashboard;
